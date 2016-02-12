ee5deb5a5d6878c83fd1ebcb7a1f2594ad00bc61
Benjamin Charity <ben@benjamincharity.com>
2016-02-12 11:38:55 -0500

    created decode filter

---
+++ src/decode.filter.js
@@ -0,0 +1,17 @@
+export function DecodeFilter() {
+
+    return decode;
+
+
+    /**
+     * Decode an encoded URL string
+     *
+     * @param {String} input
+     * @return {String} output
+     */
+    function decode(input) {
+        return decodeURI(input);
+    }
+
+}
+
