22574cfbe29f43f1fd7552aeda84dd15a8afc9b1
Benjamin Charity <ben@benjamincharity.com>
2016-02-12 11:43:45 -0500

    fixed module name format to match repo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { EncodeFilter } from './encode.filter';
 import { DecodeFilter } from './encode.filter';
 
 
-angular.module('angular-encode-url', [])
+angular.module('angular-url-encode', [])
     .filter('bpcEncode', EncodeFilter)
     .filter('bpcDecode', DecodeFilter)
 ;
